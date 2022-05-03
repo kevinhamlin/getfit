b70974c0037b26fa7e62bee2a21d52588c96b579
Amplexis <kevin.hamlin@gmail.com>
2022-05-03 11:13:42 -0400

    configuring Bootstrap

---
+++ app/javascript/js/bootstrap_js_files.js
@@ -0,0 +1,5 @@
+import 'bootstrap/js/src/tooltip'
+import 'bootstrap/js/src/modal'
+import 'bootstrap/js/src/popover'  
+
+import 'bootstrap'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,15 +7,31 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
 
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+import "../js/bootstrap_js_files"
+import * as bootstrap from 'bootstrap'
 import "../stylesheets/application"
-document.addEventListener("turbolinks:load", ()=>{
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-        $('[data-toggle="popover"]').popover()
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
     })
-})
+  
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    var modalTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="modal"]'))
+    var modalList = modalTriggerList.map(function (modalTriggerList) {
+      return new bootstrap.Tooltip(modalTriggerList)
+    })
+  });
 
 Rails.start()
 Turbolinks.start()
