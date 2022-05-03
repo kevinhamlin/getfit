3b64e75f8681bf98e7a29551c2b9df3ff9bd2974
Amplexis <kevin.hamlin@gmail.com>
2022-05-03 14:19:49 -0400

    adding testing features

---
+++ test/models/user_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(email: "unique@email.com", password: "password", password_confirmation: "password")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  # test "the truth" do
+  #   assert true
+  # end
+
+  # test "should not save user without email" do
+  #   user = User.new
+  #   assert_not user.save
+  # end
+end
